fd7553c4c6760be0bc712e2a08fe79f19357aa4f
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-11-27 09:53:03 +0530

    Adding form validation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material';
-import {MatIconModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material';
+import { MatNativeDateModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material';
+
 
 
 @NgModule({
-    imports: [
-        MatButtonModule,
-        MatIconModule],
-    exports: [
-        MatButtonModule,
-        MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule],
 })
 
 export class MaterialModule { }
