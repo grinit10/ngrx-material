d9a84d92b0a5033f1118d7c6ca70d1a210c1695d
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-12-05 20:40:11 +0530

    Adding sor, filter and pagination

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
@@ -9,7 +9,10 @@ import { TrainingService } from '../training.service';
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
 
@@ -17,5 +20,14 @@ export class PastTrainingsComponent implements OnInit {
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter = (e) => {
+    this.dataSource.filter = e.target.value.trim().toLowerCase();
   }
 }
